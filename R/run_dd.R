6231d9d4bd01a303a1ffb3c9387c91864858b6c7
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-05-26 09:35:14 -0400

    Continuer le développement de l'exécution du modèle

--- R/run_dd.R
+++ R/run_dd.R
@@ -8,10 +8,26 @@
 #' @examples run_dd()
 run_dd <- function() {
   ##
+  load(file = file.path('data', 'dd_data.RData'), verbose = 1)
+  load(file = file.path('data', 'dd_param.RData'), verbose = 1)
+  ##
   randomVal <- c('logRpred')
   ##
-  mapVal <- list(logSigma_C = factor(NA))
+  mapVal <- list(logSigma_C = factor(NA), trans_M = factor(NA))
   ##
   obj <- RTMB::MakeADFun(fnll, dd_param, random = randomVal, map = mapVal)
+  ##
+  fit <- nlminb(
+    obj$par,
+    obj$fn,
+    obj$gr,
+    control = list(eval.max = 100000000, iter.max = 100000000)
+  )
+  ##
+  sdr <- sdreport(obj)
+  pl <- as.list(sdr, "Est")
+  plsd <- as.list(sdr, "Std")
+  plr <- as.list(sdr, "Est", report = TRUE)
+  plrsd <- as.list(sdr, "Std", report = TRUE)
   ##
 }
