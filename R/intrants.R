60cb455d0f74f6be86a8f32ff50a30969cc335e1
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-05-27 12:06:57 -0400

    Modifier le format du taux de perte d'étiquettes dans les données

--- R/intrants.R
+++ R/intrants.R
@@ -13,7 +13,7 @@
 #'
 #' @examples
 calculer_intrants <- function(annee) {
-  d_init <- list()
+  dd_data <- list()
 
   ## Débaquements observés, kilogramme, année civile
   load(file.path(
@@ -30,7 +30,7 @@ calculer_intrants <- function(annee) {
     annee = temp$annee,
     valeur = temp$debarquement * 1000
   )))
-  d_init$Cobs <- Cobs
+  dd_data$Cobs <- Cobs
   ##
   write.csv2(
     Cobs,
@@ -78,7 +78,7 @@ calculer_intrants <- function(annee) {
   Bobs$source <- 1
   Bobs$sigma <- 1
   Bobs$nom <- 'indiceCombine'
-  d_init$Bobs_abs <- Bobs
+  dd_data$Bobs_abs <- Bobs
   ##
 
   ## indices d'abondance relatif
@@ -95,7 +95,7 @@ calculer_intrants <- function(annee) {
     ),
     verbose = 1
   )
-  d_init$Bobs <- data.frame(
+  dd_data$Bobs <- data.frame(
     annee = numeric(),
     valeur = numeric(),
     source = numeric(),
@@ -116,10 +116,10 @@ calculer_intrants <- function(annee) {
       'ia_gadus.csv'
     )
   )
-  temp$source <- max(d_init$Bobs$source, 0) + 1
-  temp$sigma <- max(d_init$Bobs$sigma, 0) + 1
+  temp$source <- max(dd_data$Bobs$source, 0) + 1
+  temp$sigma <- max(dd_data$Bobs$sigma, 0) + 1
   temp$nom <- 'gadus'
-  d_init$Bobs <- rbind(d_init$Bobs, temp)
+  dd_data$Bobs <- rbind(dd_data$Bobs, temp)
   ## Lady Hammond
   temp <- as.data.frame(ybarTot$LH[['classe81plus']]$pue[, c('annee', 'moy')])
   dimnames(temp)[[2]] <- c('annee', 'valeur')
@@ -134,10 +134,10 @@ calculer_intrants <- function(annee) {
       'ia_ladyhammond.csv'
     )
   )
-  temp$source <- max(d_init$Bobs$source, 0) + 1
-  temp$sigma <- max(d_init$Bobs$sigma, 0) + 1
+  temp$source <- max(dd_data$Bobs$source, 0) + 1
+  temp$sigma <- max(dd_data$Bobs$sigma, 0) + 1
   temp$nom <- 'ladyhammond'
-  d_init$Bobs <- rbind(d_init$Bobs, temp)
+  dd_data$Bobs <- rbind(dd_data$Bobs, temp)
   ## sentinelle nGSL
   nom_fichier <- file.path(
     'S:',
@@ -176,10 +176,10 @@ calculer_intrants <- function(annee) {
       'ia_sentinelleNGSL.csv'
     )
   )
-  temp$source <- max(d_init$Bobs$source, 0) + 1
-  temp$sigma <- max(d_init$Bobs$sigma, 0) + 1
+  temp$source <- max(dd_data$Bobs$source, 0) + 1
+  temp$sigma <- max(dd_data$Bobs$sigma, 0) + 1
   temp$nom <- 'senNGSL'
-  d_init$Bobs <- rbind(d_init$Bobs, temp)
+  dd_data$Bobs <- rbind(dd_data$Bobs, temp)
   ## relevé à la palangre
   nom_fichier <- file.path(
     'S:',
@@ -203,10 +203,10 @@ calculer_intrants <- function(annee) {
       'relPalangre.csv'
     )
   )
-  temp$source <- max(d_init$Bobs$source) + 1
-  temp$sigma <- max(d_init$Bobs$sigma) + 1
+  temp$source <- max(dd_data$Bobs$source) + 1
+  temp$sigma <- max(dd_data$Bobs$sigma) + 1
   temp$nom <- 'relPalangre'
-  d_init$Bobs <- rbind(d_init$Bobs, temp)
+  dd_data$Bobs <- rbind(dd_data$Bobs, temp)
   ## pue commerciale
   nom_fichier <- file.path(
     'S:',
@@ -231,17 +231,17 @@ calculer_intrants <- function(annee) {
       'pueCommCivile.csv'
     )
   )
-  temp$source <- max(d_init$Bobs$source) + 1
-  temp$sigma <- max(d_init$Bobs$sigma) + 1
+  temp$source <- max(dd_data$Bobs$source) + 1
+  temp$sigma <- max(dd_data$Bobs$sigma) + 1
   temp$nom <- 'pueCommCivil'
-  d_init$Bobs <- rbind(d_init$Bobs, temp)
+  dd_data$Bobs <- rbind(dd_data$Bobs, temp)
   ##
   ## (sentinelle sGSL)
   ##
 
   ## indice de recrutement, tranche de taille de l'indice combiné, MPO sGSL+nGSL (69-77cm, 77-84cm, 84-91cm), décallé selon nombre d'années avant recrutement
   ## par défaut, comme les données proviennent de la même source, le même sigma est utilisé
-  d_init$Robs <- data.frame(
+  dd_data$Robs <- data.frame(
     annee = numeric(),
     valeur = numeric(),
     source = numeric(),
@@ -256,7 +256,7 @@ calculer_intrants <- function(annee) {
   temp$sigma <- 1
   temp$annee <- temp$annee + 2
   temp$nom <- 'recru69a77'
-  d_init$Robs <- rbind(d_init$Robs, temp)
+  dd_data$Robs <- rbind(dd_data$Robs, temp)
   ##
   temp <- data$mpo$indiceCombine[['classe77a84']]$nue[, c('annee', 'bmcGsl')]
   dimnames(temp)[[2]] <- c('annee', 'valeur')
@@ -264,7 +264,7 @@ calculer_intrants <- function(annee) {
   temp$sigma <- 2
   temp$annee <- temp$annee + 1
   temp$nom <- "recru77a84"
-  d_init$Robs <- rbind(d_init$Robs, temp)
+  dd_data$Robs <- rbind(dd_data$Robs, temp)
   ##
   temp <- data$mpo$indiceCombine[['classe84a91']]$nue[, c('annee', 'bmcGsl')]
   dimnames(temp)[[2]] <- c('annee', 'valeur')
@@ -272,9 +272,9 @@ calculer_intrants <- function(annee) {
   temp$sigma <- 3
   temp$annee <- temp$annee
   temp$nom <- "recru84a91"
-  d_init$Robs <- rbind(d_init$Robs, temp)
+  dd_data$Robs <- rbind(dd_data$Robs, temp)
   ##
-  recru <- d_init$Robs[, c('annee', 'valeur', 'nom')]
+  recru <- dd_data$Robs[, c('annee', 'valeur', 'nom')]
   write.csv2(
     recru,
     file = file.path(
@@ -426,7 +426,7 @@ calculer_intrants <- function(annee) {
     )
   )
   ##
-  d_init$Oobs <- omega
+  dd_data$Oobs <- omega
   if (FALSE) {
     plot(
       omega[omega$source == 1, c('annee', 'valeur')],
@@ -455,7 +455,7 @@ calculer_intrants <- function(annee) {
   )]
   ## voir le graphique dans fonction graph.intrants() du fichier graph.r
   nbAnDansMoyenne <- 8 # des valeurs de 6, 10 et 12 peuvent aussi être envisagées
-  d_init$drop2010 <- mean((pue.85plus$bmcGsl / pue.81plus$bmcGsl)[
+  dd_data$drop2010 <- mean((pue.85plus$bmcGsl / pue.81plus$bmcGsl)[
     pue.85plus$annee %in%
       (2010 + c(-(nbAnDansMoyenne / 2):(nbAnDansMoyenne / 2 - 1)))
   ])
@@ -472,15 +472,15 @@ calculer_intrants <- function(annee) {
   )
   load(file = nom_fichier, verbose = 1)
   file.info(nom_fichier)$mtime
-  d_init$lpAlpha <- as.numeric(relationML$fit$plr$A / 1000)
-  d_init$lpBeta <- as.numeric(relationML$fit$plr$B)
+  dd_data$lpAlpha <- as.numeric(relationML$fit$plr$A / 1000)
+  dd_data$lpBeta <- as.numeric(relationML$fit$plr$B)
   ## poids moyen au recrutement
-  d_init$omegaK_apres2010 <- mean(relationML$l2m(85:92)) / 1000 #poids moyen estimé des taille 85a92
-  d_init$omegaK_avant2010 <- mean(relationML$l2m(81:88)) / 1000 #poids moyen estimé des taille 81a88
+  dd_data$omegaK_apres2010 <- mean(relationML$l2m(85:92)) / 1000 #poids moyen estimé des taille 85a92
+  dd_data$omegaK_avant2010 <- mean(relationML$l2m(81:88)) / 1000 #poids moyen estimé des taille 81a88
   ## poids moyen au recrutement(age-1): utilisé pour la relation ssr, donc utiliser 85cm comme définition de stock
-  d_init$omegaKmoins1 <- mean(relationML$l2m(77:84)) / 1000 #poids moyen estimé des taille 85a95
+  dd_data$omegaKmoins1 <- mean(relationML$l2m(77:84)) / 1000 #poids moyen estimé des taille 85a95
   ## poids moyen des poissons "perdus" lors du changement de taille
-  d_init$poidsMoy81a85 <- mean(relationML$l2m(81:85)) / 1000
+  dd_data$poidsMoy81a85 <- mean(relationML$l2m(81:85)) / 1000
 
   ## données de longueur à l'age
   nom_fichier <- file.path(
@@ -492,7 +492,7 @@ calculer_intrants <- function(annee) {
   )
   load(file = nom_fichier, verbose = 1)
   file.info(nom_fichier)$mtime
-  d_init$croiss <- res$longAge[, c('age', 'longueur', 'sexe')] #dans longAge, seuls les valeurs utilisables sont conservées (enlevé observateur en bas d'un certain age, extrêmes)
+  dd_data$croiss <- res$longAge[, c('age', 'longueur', 'sexe')] #dans longAge, seuls les valeurs utilisables sont conservées (enlevé observateur en bas d'un certain age, extrêmes)
 
   ## nombre de tags posés, sur individus de plus de 77cm (capturables l'année suivante)
   nom_fichier <- file.path(
@@ -512,7 +512,7 @@ calculer_intrants <- function(annee) {
   dimnames(unTagPose)[[2]] <- c('annee', 'valeur', 'nbTag')
   deuxTagPose <- cbind(tagPose[, c('X', 'deuxTagPose')], 2)
   dimnames(deuxTagPose)[[2]] <- c('annee', 'valeur', 'nbTag')
-  d_init$nTagsPoses <- rbind(unTagPose, deuxTagPose)
+  dd_data$nTagsPoses <- rbind(unTagPose, deuxTagPose)
 
   ## nombre de tag retournés au MPO, par année de pose et année de recaptrue
   nom_fichier <- file.path(
@@ -528,7 +528,7 @@ calculer_intrants <- function(annee) {
   recap <- read.csv2(file = nom_fichier, stringsAsFactors = FALSE)
   print(file.info(nom_fichier)$mtime)
   ##
-  d_init$nTagsRetourObs <- data.frame(
+  dd_data$nTagsRetourObs <- data.frame(
     anneePose = numeric(),
     anneeRecap = numeric(),
     valeur = numeric(),
@@ -546,7 +546,7 @@ calculer_intrants <- function(annee) {
           valeur = recap[recap$X == i, paste0('retour_', j)],
           source = 1
         )
-        d_init$nTagsRetourObs <- rbind(d_init$nTagsRetourObs, temp)
+        dd_data$nTagsRetourObs <- rbind(dd_data$nTagsRetourObs, temp)
       }
     }
   }
@@ -565,21 +565,18 @@ calculer_intrants <- function(annee) {
   perteTag <- read.csv2(file = fichierPerteTag, stringsAsFactors = FALSE)
   print(file.info(fichierPerteTag)$mtime)
   ## calcul du taux de perte (Lebris etal 2009)
+  perteTag <- subset(perteTag, nbAnEnMer > 0)
   temp <- table(perteTag$nbAnEnMer, perteTag$nbTagPerdu)
-  # d_init$tauxPerte <- temp[, 2] / (temp[, 2] + 2 * temp[, 1])
+  # dd_data$tauxPerte <- temp[, 2] / (temp[, 2] + 2 * temp[, 1])
   dimnames(temp)[[2]] <- c('doubleTag', 'simpleTag')
-  d_init$tauxPerte <- temp
+  dd_data$tauxPerte <- temp
 
   ## (donnees d'analyse de force de cohorte pour déterminer mortalité naturelle)
 
   save(
-    d_init,
+    dd_data,
     file = file.path(
-      'S:',
-      'Flétan',
-      'Delay-diff flétan',
-      'input',
-      annee,
+      'data',
       'dd_data.RData'
     )
   )
